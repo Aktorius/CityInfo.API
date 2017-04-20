0a4e2247cb96fdffbd0710d401ff8db383b47450
Tarik Miri <tmiri@euroffice.co.uk>
2017-04-20 17:19:44 +0100

    Add MvcOption for xml serialization

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
@@ -14,6 +15,7 @@ namespace CityInfo.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()))
                 .AddJsonOptions(o =>
                 {
                     if(o.SerializerSettings.ContractResolver != null)
