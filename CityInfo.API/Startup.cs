9f8dcbc77b006469ac01b2666f026a20673942a4
Tarik Miri <tmiri@euroffice.co.uk>
2017-04-21 14:57:21 +0100

    Add Seed to populate the DB

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -53,7 +53,7 @@ namespace CityInfo.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, CityInfoContext cityInfoContext)
         {
             loggerFactory.AddConsole();
             loggerFactory.AddDebug();
@@ -69,6 +69,8 @@ namespace CityInfo.API
             {
                 app.UseExceptionHandler();
             }
+
+            cityInfoContext.EnsureSeedDataForContext();
 
             app.UseStatusCodePages();
             app.UseMvc();
