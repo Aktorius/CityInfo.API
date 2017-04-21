e3fa93ad0d130e3abedf8939a6f98d016b671044
Tarik Miri <tmiri@euroffice.co.uk>
2017-04-21 16:39:31 +0100

    replace code to use repository in CitiesController

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
 namespace CityInfo.API.Controllers
@@ -6,22 +8,64 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : Controller
     {
+        private ICityInfoRepository _cityInfoRepository;
+
+        public CitiesController(ICityInfoRepository cityInfoRepository)
+        {
+            _cityInfoRepository = cityInfoRepository;
+        }
+
         [HttpGet()]
         public IActionResult GetCities()
         {
-            return Ok(CitiesDataStore.Current.Cities);
+            var cities = _cityInfoRepository.GetCities();
+
+            var results = cities.Select(c => new CityWithoutPointsOfInterestDto()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Description = c.Description
+            });
+             
+            return Ok(results);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetCity(int id)
+        public IActionResult GetCity(int id, bool includePointsOfInterest = false)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            var city = _cityInfoRepository.GetCity(id, includePointsOfInterest);
             if(city == null)
             {
                 return NotFound();
             }
+            
+            if (includePointsOfInterest)
+            {
+                var cityResult = new CityDto()
+                {
+                    Id = city.Id,
+                    Name = city.Name,
+                    Description = city.Description
+                };
 
-            return Ok(city);
+                cityResult.PointsOfInterest = city.PointsOfInterest.Select(poi => new PointOfInterestDto()
+                {
+                    Id = poi.Id,
+                    Name = poi.Name,
+                    Description = poi.Description
+                }).ToList();
+                
+                return Ok(cityResult);
+            }
+
+            var cityWithoutPointsOfInterestResult = new CityWithoutPointsOfInterestDto()
+            {
+                Id = city.Id,
+                Name = city.Name,
+                Description = city.Description
+            };
+
+            return Ok(cityWithoutPointsOfInterestResult);
         }
     }
 }
