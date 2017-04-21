b4fdc6b5fe7b75538c71d1d016391853a5a4908d
Tarik Miri <tmiri@euroffice.co.uk>
2017-04-21 15:44:49 +0100

    Implement a repository for cities

---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+using CityInfo.API.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private CityInfoContext _cityInfoContext;
+
+        public CityInfoRepository(CityInfoContext cityInfoContext)
+        {
+            _cityInfoContext = cityInfoContext;
+        }
+
+        public IEnumerable<City> GetCities()
+        {
+            return _cityInfoContext.Cities.OrderBy(c => c.Name).ToList();
+        }
+
+        public City GetCity(int cityId, bool includePointsOfInterest)
+        {
+            if(includePointsOfInterest)
+                return _cityInfoContext.Cities.Include(c => c.PointsOfInterest)
+                        .Where(c => c.Id == cityId).FirstOrDefault();
+
+            return _cityInfoContext.Cities.Where(c => c.Id == cityId).FirstOrDefault();
+        }
+
+        public PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId)
+        {
+            return _cityInfoContext.PointsOfInterest.Where(p => p.City.Id == cityId && p.Id == pointOfInterestId).FirstOrDefault();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId)
+        {
+            return _cityInfoContext.PointsOfInterest.Where(p => p.City.Id == cityId).ToList();
+        }
+    }
+}
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -50,6 +50,8 @@ namespace CityInfo.API
 #endif
             var connectionString = Configuration["connectionStrings:CityInfoConnectionString"];
             services.AddDbContext<CityInfoContext>(o => o.UseSqlServer(connectionString));
+
+            services.AddScoped<ICityInfoRepository, CityInfoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
